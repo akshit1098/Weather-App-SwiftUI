29ac5ed7eefdee6a4a339070d595195ca390547a
akshit1098 <saxena.aks@northeastern.edu>
2024-11-30 22:37:58 -0500

    Initial layout is completed

---
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Akshit Saxena on 11/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello world!")
+                Button(action: {
+                    // Your action code here
+                }) {
+                    Text("MyButton")
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Akshit Saxena on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
