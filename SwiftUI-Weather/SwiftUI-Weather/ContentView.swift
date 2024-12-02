1062215a93cb73f4d55d0a4409baae98526682d5
akshit1098 <saxena.aks@northeastern.edu>
2024-12-02 01:24:11 -0500

    Added linear gradient and initial layout

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -9,19 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello world!")
-                Button(action: {
-                    // Your action code here
-                }) {
-                    Text("My Button")
-                }
+        ZStack {
+            // Intially with just the color, safe area would be spared from the colors
+            // But when we do the .ignoreSafeArea, it will color that area also
+//            Color(.blue)
+//                .ignoresSafeArea(.all)
+            
+            // order of modifier(fon,styles, etc) matter
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .ignoresSafeArea(.all)
+            VStack{
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                    .padding() //adds padding within the text
+                
+                Spacer() //using spacer will extend Vstack and push the text on the top. Spacers fill the entire spcae
+
             }
-            .padding()
         }
     }
 }
