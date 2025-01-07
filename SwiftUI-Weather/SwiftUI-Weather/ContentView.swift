7d57d70c4e988742ecb813bdce446ef681cff6b5
akshit1098 <saxena.aks@northeastern.edu>
2025-01-07 20:50:47 +0530

    Added New Button and layouts

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -16,30 +16,33 @@ struct ContentView: View {
 //                .ignoresSafeArea(.all)
             
             // order of modifier(fon,styles, etc) matter
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-                .ignoresSafeArea(.all)
+            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
             VStack{
-                Text("Cupertino, CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundStyle(.white)
-                    .padding() //adds padding within the text
+                CityTextView(cityName: "Cupertino, CA")
+
+                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
                 
-                VStack(spacing: 8){
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit) // So that the image does not appear to be stretched
-                        .frame(width: 180, height: 180) // The frame to adjust height and the width of the image
-                    Text("76°") //option+shift+8 = °
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundStyle(.white)
-                    
+                HStack(spacing: 20){
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 74)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "wind.snow", temperature: 55)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 60)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "snow", temperature: 25)
                 }
                 
                 Spacer() //using spacer will extend Vstack and push the text on the top. Spacers fill the entire spcae
-
+                
+                Button{
+                    print("Tapped")
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
             }
         }
     }
@@ -47,4 +50,83 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundStyle(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit) // So that the image does not appear to be stretched
+                .frame(width: 40, height: 40) // The frame to adjust height and the width of the image
+            Text("\(temperature)") //option+shift+8 = °
+                .font(.system(size: 28, weight: .medium))
+                .foregroundStyle(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    var topColor: Color
+    var bottomColor: Color
+    var body: some View {
+        
+      
+        
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .ignoresSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundStyle(.white)
+            .padding() //adds padding within the text
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8){
+            Image(systemName: "cloud.sun.fill")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit) // So that the image does not appear to be stretched
+                .frame(width: 180, height: 180) // The frame to adjust height and the width of the image
+            Text("76°") //option+shift+8 = °
+                .font(.system(size: 70, weight: .medium))
+                .foregroundStyle(.white)
+            
+        }
+        
+        .padding(.bottom, 40)
+    }
+}
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    var body: some View {
+        
+    }
 }
